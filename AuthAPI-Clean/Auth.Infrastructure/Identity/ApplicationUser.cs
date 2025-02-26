dbef0ecc585436fb31f8e21e8395689fdf0ac62c
Hadi Bawarshi <81911956+HadiBawarshi@users.noreply.github.com>
2025-02-26 21:37:05 +0200

    migrating and updating db to create the identity tables

--- AuthAPI-Clean/Auth.Infrastructure/Identity/ApplicationUser.cs
+++ AuthAPI-Clean/Auth.Infrastructure/Identity/ApplicationUser.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace Auth.Infrastructure.Identity
 {
     public class ApplicationUser : IdentityUser
     {
+        [StringLength(254)]
         public string? Name { get; set; }
 
     }
