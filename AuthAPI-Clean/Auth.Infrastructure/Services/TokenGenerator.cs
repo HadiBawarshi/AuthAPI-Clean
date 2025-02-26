72818b0892d305dfd1cbba9423777e9034a3d15c
Hadi Bawarshi <81911956+HadiBawarshi@users.noreply.github.com>
2025-02-26 21:12:48 +0200

    adding the needed appsetting.json objects and addking token generator service

---
+++ AuthAPI-Clean/Auth.Infrastructure/Services/TokenGenerator.cs
@@ -0,0 +1,41 @@
+﻿using Auth.Infrastructure.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Auth.Infrastructure.Services
+{
+    public sealed class TokenGenerator
+    {
+        private readonly IConfiguration _configuration;
+
+        public TokenGenerator(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        public string GenerateJwtTokenAsync(ApplicationUser user)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Name, user.UserName)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtSettings:Secret"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JwtSettings:Issuer"],
+                audience: _configuration["JwtSettings:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
